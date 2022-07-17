02e5da23614a0367d2d3167b6f3ab05507002455
joon sim <wesley0940@gmail.com>
2022-07-17 20:09:55 +0900

    1_1

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 35px;
+    padding: 1rem;
+    color: white;
+    background: teal;
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+
+function Header(){
+    return(
+    <>
+        <Wrapper>sadsad</Wrapper>
+    </>
+    )
+}
+
+export default Header;
